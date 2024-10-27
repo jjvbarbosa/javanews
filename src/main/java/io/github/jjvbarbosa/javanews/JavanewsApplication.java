f9f87e284452caff739f36fdf1fd2cc15a169f24
jjvbarbosa <joaovitor123war@hotmail.com>
2024-10-27 17:27:23 -0300

    🎉: first commit

---
+++ src/main/java/io/github/jjvbarbosa/javanews/JavanewsApplication.java
@@ -0,0 +1,13 @@
+package io.github.jjvbarbosa.javanews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavanewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavanewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jjvbarbosa/javanews/JavanewsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jjvbarbosa.javanews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavanewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
